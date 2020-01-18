c994ef86a35bb7b60b1da8604e16641d7968f041
Jeyssiva <48796522+Jeyssiva@users.noreply.github.com>
2020-01-18 22:25:07 +0530

    Update baseController.js
    file upload

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,71 +1,29 @@
 const User = require('../models/user')
-const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: './uploads/',
-    filename: function(req, file, cb){
-      cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-  });
-  
-  // Init Upload
-  const uploadFile = multer({
-    storage: storage,
-    limits:{fileSize: 1000000},
-    fileFilter: function(req, file, cb){
-        checkFileFormat(file, cb);
-    }
-  }).single('profileIcon');
-
-function checkFileFormat(file,cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if(mimetype && extname){
-    return cb(null,true);
-  } else {
-    cb('Error: Images Only!');
-  }
-}
 
 exports.login = (req,res) =>{
     res.render('login' , {errors : false})
 }
 
 exports.signup = (req,res) => {
-    res.render('signup')
+    res.render('signup', {errors : false})
 }
 
 exports.saveSignup = (req,res) => {
-   
-    // uploadFile(req,res ,(err)=>{
-    //     console.log("iconUpload" ,req.file)
-    //     //res.send({msg:"upload"})
-    // });
-
-
     var newUser = User({
         firstname : req.body.first_name,
         lastname : req.body.last_name,
         email : req.body.email,
-        password : req.body.password
+        password : req.body.password,
+        iconpath : req.file.path
     })
 
     newUser.save(err => {
-        if(err) 
-        {
-            res.status(400).send({err : err })
+        if(err) {
+          res.json({success : false , msg :err.msg})
         } else {
-            res.status.redirect('/')
+          res.json({success : true , msg :"null"})
         }
     })
-
-    // req.db.collection("users").insert(req.body,  (err, result) => {
-    //     if (err) throw err;
-    //     res.render('login')
-    //  });
 }
 
 exports.checkLogin = (req,res) => {
@@ -82,7 +40,8 @@ exports.checkLogin = (req,res) => {
                req.session.userdetails =  {
                 firstname : result.firstname,
                 lastname : result.lastname,
-                email : result.email
+                email : result.email,
+                iconpath : result.iconpath
                }
                console.log("req.session.userdetails" , req.session.userdetails)
                res.redirect('/profile')
@@ -99,19 +58,6 @@ exports.checkLogin = (req,res) => {
 
 exports.profile = (req,res) => {
   res.render('profile')
-}
-
-exports.iconUpload = (req,res)=>{
-    console.log("req.body.ffile" , req.body)
-    console.log("req.body.ffile" , req.files)
-    req.session.selectedIcon = req.body.file
-  //  console.log(req.session.selectedIcon)
-
-    // console.log("iconUplod-main" , req.body)
-    // uploadFile(req,res ,(err)=>{
-    //     console.log("iconUpload" ,req.file)
-    //     //res.send({msg:"upload"})
-    // });
 }
 
 exports.logout = (req, res) => {
